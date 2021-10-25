30c6686bc197cd33b420d7258d6afcc57dd768f0
lingqiao <2676272310@qq.com>
2021-10-25 16:27:15 +0800

    init

---
+++ day01_restfulDemo/src/main/java/com/example/day01_restfuldemo/Day01RestfulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.day01_restfuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01RestfulDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day01RestfulDemoApplication.class, args);
+    }
+
+}
---
+++ day01_restfulDemo/src/test/java/com/example/day01_restfuldemo/Day01RestfulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day01_restfuldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day01RestfulDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
